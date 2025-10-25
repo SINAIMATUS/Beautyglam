b89d0c4cbda293089e1b287ba120ef813909c498
SINAIMATUS <matusdayanara355@gmail.com>
2025-10-25 01:30:51 -0600

    Primera Actualizacion

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { View, Text, FlatList, StyleSheet } from "react-native";
+import { db } from "./src/database/firebaseconfig"
+import { collection, getDocs } from "firebase/firestore";
+
 
 export default function App() {
+  const [Productos, setProductos] = useState([]);
+
+  useEffect(() => {
+    const fetData = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "Productos"));
+        const data = querySnapshot.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data(),
+
+        }));
+        setProductos(data);
+      } catch (error) {
+        console.error("Error al obtener documentos:", error);
+      }
+    };
+    fetData();
+  }, []);
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={StyleSheet.cotainer}>
+      <Text style={styles.titulo}>Lista de Productos</Text>
+      <FlatList
+        data={Productos}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <Text style={styles.item}>
+            {item.Nombre} - ${item.Precio} - {item.Descripcion} - {item.CodigoDeProducto} - {item.Categoria}
+          </Text>
+        )}
+      />
     </View>
-  );
+
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "flex-start",
+    paddingTop: 20,
+    paddingHorizontal: 20,
+  },
+  titulo: {
+    fontSize: 22,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  item: {
+    fontSize: 18,
+    marginBottom: 5
   },
 });
